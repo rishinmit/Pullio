1841fa445bc17fb8a15e8d6e4ae68b230653141a
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2025-03-15 19:30:51 +0530

    most recent commit made by rishi

--- second.java
+++ second.java
@@ -1,6 +1,10 @@
 public class second {
     public static void main(String[] args) {
+        //array by tej
         int arr[]={1,2,3,4,5};
         System.out.println(arr);
+        //array by rishi
+        int arr2[]={6,7,8,9};
+        System.out.println(arr2);
     }
 }
